49f74b98858ea7c35f9160889e785bb98e052490
heuav <taylorwboyer@gmail.com>
2022-06-29 15:18:26 -0600

    Second Commit
    Added initial placeholder content

---
+++ index.js
@@ -0,0 +1,10 @@
+let twitterBtn = document.getElementById("twitter-btn")
+let twitterIcon = document.getElementById("twitter-icon")
+
+twitterBtn.addEventListener("click", function(){
+    window.open("https://twitter.com/0xheuav")
+})
+
+function linkToPage(link){
+    window.open(link)
+}
